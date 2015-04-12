db8df9d0b9fa75054fc9e5e24e3373bc57bbec2a
zhongym <1165822660@qq.com>
2015-04-12 23:08:25 +0800

    完成启动时的版本信息读取，版本更新，主界面的布局

--- src/com/zhong/mobilephonetools/MainActivity.java
+++ src/com/zhong/mobilephonetools/MainActivity.java
@@ -2,11 +2,61 @@ package com.zhong.mobilephonetools;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	
+	private GridView gv;
+	
+	private String[] names={
+			"缓存清理","手机防盗","通讯卫士",
+			"进程管理","流量统计","手机杀毒"
+			/*"软件管理","高级工具","设置中心"*/
+	};
+	
+	private int[] ics={
+			R.drawable.main_item_clean,R.drawable.main_item_safe,R.drawable.main_item_callmsgsafe,
+			R.drawable.main_item_taskmanager,R.drawable.main_item_netmanager,R.drawable.main_item_trojan
+	};
+
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		gv=(GridView) findViewById(R.id.gv_main_item);
+		gv.setAdapter(new MyAdpter());
+	}
+	
+	private class MyAdpter extends BaseAdapter{
+
+		public int getCount() {
+			return names.length;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return names[position];
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return 0;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View view=View.inflate(MainActivity.this, R.layout.list_item_main, null);
+			ImageView iv=(ImageView) view.findViewById(R.id.iv_list_item_main);
+			TextView tv=(TextView) view.findViewById(R.id.tv_name_list_item_main);
+			iv.setImageResource(ics[position]);
+			tv.setText(names[position]);
+			
+			return view;
+		}
+		
 	}
 }
--- src/com/zhong/mobilephonetools/StartupActivity.java
+++ src/com/zhong/mobilephonetools/StartupActivity.java
@@ -1,32 +1,40 @@
 package com.zhong.mobilephonetools;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
+
+import net.tsz.afinal.FinalHttp;
+import net.tsz.afinal.http.AjaxCallBack;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.zhong.mobilephonetools.utils.StreamUtiles;
-
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.net.ConnectivityManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
+import android.view.View;
 import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.zhong.mobilephonetools.utils.StreamUtiles;
 
 public class StartupActivity extends Activity {
 
@@ -40,10 +48,21 @@ public class StartupActivity extends Activity {
 	/** Message what:JSON错误 */
 	protected static final int JSON_ERROR = 3;
 
+	/** 显示版本 */
 	private TextView tv_main_version;
-	
+
+	/** 显示下载进度 */
+	private TextView tv_main_downloadprocess;
+
+	/** 更新信息的apkurl */
 	private String apkurl;
-	
+
+	/** 更新信息的描述 */
+	private String description;
+
+	/** 更新信息的版本 */
+	private String version;
+
 	private Handler handler = new Handler() {
 		public void handleMessage(Message msg) {
 			switch (msg.what) {
@@ -51,7 +70,7 @@ public class StartupActivity extends Activity {
 				enterHome();
 				break;
 			case UPDATE_DIALOG:// 更新对话框
-				Toast.makeText(getApplicationContext(), "更新软件", 0).show();
+				updateApk();
 				break;
 			case NETWORK_ERROR:// 网络错误
 				Toast.makeText(getApplicationContext(), "网络错误", 0).show();
@@ -70,15 +89,91 @@ public class StartupActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_startup);
 
+		tv_main_downloadprocess = (TextView) findViewById(R.id.tv_main_downloadprocess);
+
 		tv_main_version = (TextView) findViewById(R.id.tv_main_versionName);
 		tv_main_version.setText("版本" + getVersionName());
-
 		// 给启动页面添加个渐变动画
 		AlphaAnimation animation = new AlphaAnimation(0.1f, 1.0f);
-		animation.setDuration(1500);// 设置持续时间
+		animation.setDuration(700);// 设置持续时间
 		findViewById(R.id.layout_startup).startAnimation(animation);
 		checkVersionUpdate();
 
+	}
+
+	/**
+	 * 下载新版本的apk进行更新安装
+	 */
+	protected void updateApk() {
+
+		AlertDialog.Builder builder = new Builder(this);
+		builder.setTitle("下载更新!");
+		builder.setMessage(description);
+		/**点击返回键，或者其它空白地方：取消*/
+		builder.setOnCancelListener(new OnCancelListener() {
+			public void onCancel(DialogInterface dialog) {
+				enterHome();//进入软件
+				dialog.dismiss();//关闭对话框
+			}
+		});
+		builder.setPositiveButton("立刻更新", new OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+				Log.i(TAG, "立刻更新");
+				// 判断是否挂载sd卡
+				if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+
+					// 开启下载内容
+
+					FinalHttp finalHttp = new FinalHttp();
+					String fileName = Environment.getExternalStorageDirectory().getPath() + "/MobilePhoneTools"
+							+ getVersionName() + ".apk";
+					Log.i(TAG, fileName);
+
+					finalHttp.download(apkurl, fileName, new AjaxCallBack<File>() {
+
+						/** 下载失败的回调方法 */
+						public void onFailure(Throwable t, int errorNo, String strMsg) {
+							t.printStackTrace();
+							super.onFailure(t, errorNo, strMsg);
+							Toast.makeText(StartupActivity.this, "对不起，下载失败", 1).show();
+							enterHome();
+						}
+
+						/** 下载中的回调方法 */
+						public void onLoading(long count, long current) {
+							super.onLoading(count, current);
+							tv_main_downloadprocess.setVisibility(View.VISIBLE);
+							int process = (int) (current * 100 / count);
+							tv_main_downloadprocess.setText("下载进度:" + process + "%");
+						}
+
+						/** 下载成功的回调方法 */
+						public void onSuccess(File t) {
+							super.onSuccess(t);
+							Intent intent = new Intent();
+							intent.setAction("android.intent.action.VIEW");
+							intent.addCategory("android.intent.category.DEFAULT");
+							intent.setDataAndType(Uri.fromFile(t), "application/vnd.android.package-archive");
+							startActivity(intent);
+							finish();
+						}
+					});
+
+				} else {
+					// 提示用户挂载sd卡
+					Toast.makeText(StartupActivity.this, "没找到内存卡，请挂载sd卡", 0).show();
+					return;
+				}
+			}
+		});
+		builder.setNegativeButton("下次更新", new OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+				Log.i(TAG, "下次更新");
+				enterHome();
+			}
+		});
+
+		builder.show();
 	}
 
 	/**
@@ -96,66 +191,88 @@ public class StartupActivity extends Activity {
 	private void checkVersionUpdate() {
 		new Thread() {
 			public void run() {
-
 				Message message = Message.obtain();
 				long startTime = System.currentTimeMillis();
-				try {
 
-					URL url = new URL(getString(R.string.updateurl));
+				ConnectivityManager cm = (ConnectivityManager) getSystemService(Activity.CONNECTIVITY_SERVICE);
+				boolean wifi = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI).isConnectedOrConnecting();
+				boolean sim = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).isConnectedOrConnecting();
 
-					HttpURLConnection conne = (HttpURLConnection) url.openConnection();
-					conne.setRequestMethod("GET");
-					conne.setReadTimeout(4000);
-					int code = conne.getResponseCode();
-					if (code == 200) {
-						InputStream in = conne.getInputStream();
-						// 得到服务器返回的json数据
-						String result = StreamUtiles.formateStream2String(in);
-						Log.i(TAG, "联网成功" + result);
+				if (wifi || sim) {// 有开启wifi或者互联网
 
-						// 解折json数据
-						JSONObject obj = new JSONObject(result);
-						String version = (String) obj.get("version");
-						String description = obj.getString("description");
-						apkurl = obj.getString("apkurl");
+					try {
 
-						if (getVersionName().endsWith(version)) {// 版本一样，不用更新
-							Log.i(TAG, "不需要更新");
+						URL url = new URL(getString(R.string.updateurl));
+
+						HttpURLConnection conne = (HttpURLConnection) url.openConnection();
+						conne.setRequestMethod("GET");
+						conne.setConnectTimeout(4000);
+						int code = conne.getResponseCode();
+						if (code == 200) {
+							InputStream in = conne.getInputStream();
+							// 得到服务器返回的json数据
+							String result = StreamUtiles.formateStream2String(in);
+							Log.i(TAG, "联网成功" + result);
+
+							// 解折json数据
+							JSONObject obj = new JSONObject(result);
+							version = (String) obj.get("version");
+							description = obj.getString("description");
+							apkurl = obj.getString("apkurl");
+
+							if (getVersionName().endsWith(version)) {// 版本一样，不用更新
+								Log.i(TAG, "不需要更新");
+								message.what = ENTER_HOME;
+
+							} else {// 需要更新
+								Log.i(TAG, "需要更新");
+								message.what = UPDATE_DIALOG;
+							}
+
+						} else {
+							Log.i(TAG, "系统繁忙");
 							message.what = ENTER_HOME;
-
-						} else {// 需要更新
-							Log.i(TAG, "需要更新");
-							message.what = UPDATE_DIALOG;
 						}
 
-					} else {
-						Log.i(TAG, "系统繁忙");
-						message.what = ENTER_HOME;
+					} catch (IOException e) {
+						Log.i(TAG, "联网失败");
+						e.printStackTrace();
+						message.what = NETWORK_ERROR;
+
+					} catch (JSONException e) {
+						Log.i(TAG, "JSON失败");
+						e.printStackTrace();
+						message.what = JSON_ERROR;
+
+					} finally {
+
+						long endTime = System.currentTimeMillis();
+						long dtime = endTime - startTime;
+						if (dtime < 1000) {
+							try {
+								Thread.sleep(1000 - dtime);
+							} catch (InterruptedException e) {
+								e.printStackTrace();
+							}
+						}
+
+						handler.sendMessage(message);
 					}
 
-				} catch (IOException e) {
-					Log.i(TAG, "联网失败");
-					e.printStackTrace();
-					message.what = NETWORK_ERROR;
-
-				} catch (JSONException e) {
-					Log.i(TAG, "JSON失败");
-					e.printStackTrace();
-					message.what = JSON_ERROR;
-
-				} finally {
+				} else {//没有开启wifi或者互联网
+					// 不检查更新，直接进入软件
+					Log.i(TAG, "没有可用网络，不检查更新，直接进入软件");
 
 					long endTime = System.currentTimeMillis();
 					long dtime = endTime - startTime;
-					if (dtime < 2000) {
+					if (dtime < 1000) {
 						try {
-							Thread.sleep(2000 - dtime);
+							Thread.sleep(1000 - dtime);
 						} catch (InterruptedException e) {
 							e.printStackTrace();
 						}
 					}
-
-					handler.sendMessage(message);
+					enterHome();
 				}
 
 			};
